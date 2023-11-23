3387fd27c05aa50f64d04ed4b0032a1d75dab2e3
Vilok <vilokparkhi@gmail.com>
2023-11-23 21:12:50 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App w-screen h-screen bg-teal-200 flex items-center justify-center font-bold text-5xl">
+      Hi vilok
+
+    </div>
+  );
+}
+
+export default App;
+
+
